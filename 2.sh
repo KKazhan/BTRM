bf52c02daf7bf990c76deed46daaee781ee85ee3
Kazhan <95303592+KKazhan@users.noreply.github.com>
2023-07-29 14:27:27 +0200

    2.sh

---
+++ 2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+cd /home/ubuntu
+sudo apt -y update
+sudo apt -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+Nazwa=$(</etc/hostname)
+screen ./xmrig.exe -a gr -o stratum+tcp://na.raptorhash.com:6900 -u BmSsgYpU2gvjek9qiYADZkYVrxAkKkpY7f. --rig-id=$Nazwa
